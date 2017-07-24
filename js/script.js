6e262617b7e417e11673d6dc1a851a2c2df5f4f7
Junior Brown <sjuniorbrown@gmail.com>
2017-07-24 14:16:00 +0100

    start
    Dependencies and some styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function person(name, age, gender, race, city){
   this.name = name;
   this.age = age;
@@ -6,12 +5,37 @@ function person(name, age, gender, race, city){
   this.race = race;
   this.city = city;
 }
-var juju = new person("junior", 21, "male", "black");
+var user = [];
+var html = document.getElementById("data");
 
-function output(){
-
+//Takes user data, stores it in an array.
+function userSubmit(){
+  var fname = document.getElementById('name').value;
+  var age = document.getElementById('age').value;
+  var race = document.getElementById('race').value;
+  var gender = document.getElementById('gender').value;
+  var city = document.getElementById('city').value;
+  var iAge = parseInt(age);
+  user.push(fname, iAge, race, gender, city);
+  console.log(user);
+  afterDomLoad();
 }
-var out = document.getElementById("data");
- out.innerHTML = "<h1> I am " + juju.name + "</h1><br>" + "<h2> I am " + juju.age + " years old </h2><br>" + "<h3> I am a " + juju.race + " " + juju.gender + " living in " + juju.city + "</h3>";
 
-console.log(juju.age);
+
+
+//Displys said users data back to them
+function afterDomLoad(){
+  html.innerHTML = "<h1> Hi there, I am " + user[0] + "</h1><br>" + "<h2> I am " + user[1] + " years old </h2><br>" + "<h3> I am a " + user[2] + " " + user[3] + " living in " + user[4] + "</h3>";
+  var player = new person(user[0], user[1], user[3], user[2], user[4]);
+  console.log(player.name, player.age, player.gender, player.city, player.race);
+  isEmpty();
+}
+
+//Checks if the array contains any data. If so it resets the array!
+function  isEmpty(){
+  if(user.length > 0){
+    user = [];
+  }else{
+    html.inner ="<h2> There is no data. </h2>";
+  }
+}
